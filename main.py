186be509858e47ebf80d4c2355a9b51d28d9655b
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-01-27 10:30:30 -0600

    Step 3 - Create player behavior

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+new_player = Player()
+
+screen.listen()
+screen.onkey(new_player.move_forward, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    screen.exitonclick()
